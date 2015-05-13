02c5e44623a0dcbd0de8b4960b24ff98b7dd282e
NaderTch <nader.khedhri@technetmedia.ch>
2015-05-13 10:30:58 +0100

    Profile ajouté avec succès

--- IOS-Objective-c/IOS-Objective-c/ViewController.m
+++ IOS-Objective-c/IOS-Objective-c/ViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
+
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize firstName, lastName, birthDay, sex;
+
+NSString *sexInput = @"male";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,5 +28,42 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)changeText:(id)sender {
+    if (sex.selectedSegmentIndex == 0) {
+        sexInput = @"male";
+    }
+    if (sex.selectedSegmentIndex == 1) {
+        sexInput = @"female";
+    }
+}
+
+- (IBAction)signIn:(id)sender {
+    
+    NSString *firstNameInput = firstName.text;
+    NSString *lastNameInput = lastName.text;
+    NSDate *birthDayInput = [birthDay date];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    NSString *stringDate =[formatter stringFromDate:birthDayInput];
+    
+    NSLog(@"%@, %@, %@, %@", firstNameInput, lastNameInput, stringDate, sexInput);
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    NSDictionary *params = @ {@"firstName" :firstNameInput, @"lastName" :lastNameInput, @"birthDay" :stringDate, @"sex" :sexInput };
+    
+    [manager POST:@"http://192.168.1.2/ios/public/profiles" parameters:params
+          success:^(AFHTTPRequestOperation *operation, id responseObject)
+    {
+        NSLog(@"JSON: %@", responseObject);
+    }
+          failure:
+     ^(AFHTTPRequestOperation *operation, NSError *error) {
+         NSLog(@"Error: %@", error);
+     }];
+}
+
 
 @end
